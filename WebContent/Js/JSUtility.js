f139885854493c58dd3c83ccd3e8c080c4a519b6
srivenkatesh <srivenkatesh@LAPTOP-624SRL4R>
2017-03-19 22:25:58 +0530

    first commit - javascript email validation v1.0.0

--- WebContent/Js/JSUtility.js
+++ WebContent/Js/JSUtility.js
@@ -1,3 +1,27 @@
 /**
- * 
+ *  JSUtility - v1.0.1
+ *  https://github.com/srivenkatesh92/JavascriptUtility
+ *  2017-03-19
+ *  
+ *  Freely distributable
+ *  copyright (c) 2017 Srivenkatesh
+ *  Email : srivenkatesh123@gmail.com
+ *  
  */
+
+ function JSUtility(){};
+ 
+ /**
+  * emailValidation
+  * 
+  * This method return true/false depends on valid/invalid
+  * valid email : someemail@domain.com
+  * invalid email : someemail@domaildasd
+  * 
+  */
+ var emailValidation = function(email){	    
+	        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	        return re.test(email);	   
+ };
+ 
+ 
