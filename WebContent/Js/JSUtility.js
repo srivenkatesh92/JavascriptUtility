557bf3e54e0c5df70cfb6666ffcbe47518fee62c
srivenkatesh <srivenkatesh@LAPTOP-624SRL4R>
2017-04-14 11:53:13 +0530

    Only Allow Alphabets validation

--- WebContent/Js/JSUtility.js
+++ WebContent/Js/JSUtility.js
@@ -48,3 +48,26 @@
 	 
 	 return res;
  };
+ 
+ /**
+  * Only Allow Alphabets. Eg:  onkeypress="return onlyAlphabets(event,this);"
+  * 
+  */
+ var alphaOnly = function (e, t) {
+	    try {
+	        if (window.event) {
+	            var charCode = window.event.keyCode;
+	        }
+	        else if (e) {
+	            var charCode = e.which;
+	        }
+	        else { return true; }
+	        if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || (charCode == 32 || charCode == 46))
+	            return true;
+	        else
+	            return false;
+	    }
+	    catch (err) {
+	        alert(err.Description);
+	    }
+	}
